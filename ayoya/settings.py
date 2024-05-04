4df4a0e82597fd775108084fac90bdc6106cc9dd
sanjay <sanjaypython22@gmail.com>
2024-05-04 17:23:57 +0530

    p

--- ayoya/settings.py
+++ ayoya/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.static',
+    'django.contrib.staticfiles',
     'homepage',
 ]
 
