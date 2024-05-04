303bf05be7276cbd5e8892b5c62d75847b274c2a
sanjay <sanjaypython22@gmail.com>
2024-05-04 17:10:09 +0530

    p

--- ayoya/settings.py
+++ ayoya/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.static',
     'homepage',
 ]
 
@@ -106,7 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
